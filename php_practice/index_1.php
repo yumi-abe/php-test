9f6552f582f1b7a94616b203137a8a6b90a1ccf6
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-03-08 15:31:30 +0900

    関数部分追加

--- php_practice/index_1.php
+++ php_practice/index_1.php
@@ -26,5 +26,92 @@ function getComment($string){
 
 getComment($comment);
 
+echo '<br>';
+
+
+//インプット引数なし、アウトプット戻りあり
+
+function getNumberOfComment(){
+    return 5;
+}
+var_dump(getNumberOfComment());
+echo getNumberOfComment();
+
+
+echo '<br>';
+//インプット引数2つ、アウトプット戻りあり
+
+function sumPraice($int1, $int2){
+    $int3 = $int1 + $int2;
+    return $int3;
+}
+
+$total = sumPraice(3,5);
+echo $total;
+
+echo '<br>';
+echo '<br>';
+
+
 ?>
+
+・組み込み関数
+
+<?php
+
+echo '<br>';
+
+//文字列の長さ
+//マルチバイト…日本語SJIS, UTF-8 3~6バイト（1文字3バイト）
+
+
+$text = 'あいうえお';
+
+echo strlen($text);
+
+echo '<br>';
+
+echo mb_strlen($text);
+
+echo '<br>';
+
+//文字列の置換
+$str = '文字列を置換します';
+echo str_replace('置換','ちかん',$str);
+
+echo '<br>';
+
+//指定文字列で分割
+
+$str_2 = '指定文字列で、分割します';
+
+var_dump(explode('、', $str_2));
+
+echo '<br>';
+
+//文字列をくっつける…implode
+
+//正規表現…文字かどうか、数字かどうか、郵便番号・メールアドレスなど
+
+$str_3 = '特定の文字列が含まれるか確認する';
+echo preg_match('/文字列/',$str_3);
+
+echo '<br>';
+
+//指定文字列から文字列を取得する
+
+echo substr('abcde', 2);
+
+echo '<br>';
+
+echo mb_substr('あいうえお', 2);
+
+echo '<br>';
+
+//配列に配列を追加する
+
+
+
+?>
+
 
