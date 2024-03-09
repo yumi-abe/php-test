de694f137586654645c0ca327ff9e1117db98a2a
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-03-09 20:02:14 +0900

    section2-1

---
+++ section2/form.php
@@ -0,0 +1,21 @@
+<?php
+
+// phpinfo();
+//display_errors がOFFになっているとエラーがでない
+
+?>
+
+<?php
+
+//処理を止める関数
+$test = 123;
+$test2 = 456;
+
+echo $test;
+var_dump($test);
+exit; //処理を止める
+echo $test2;
+
+
+
+?>
