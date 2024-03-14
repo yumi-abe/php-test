83fee6a8499a52e60369dfaef2ddf38539ebf9b2
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-03-14 14:13:35 +0900

    抽象クラスとインターフェイス

---
+++ object/interface.php
@@ -0,0 +1,89 @@
+<?php
+
+interface ProductInterface{
+  //変数　関数
+  // public function echoProduct(){
+  //   echo '親クラスです';
+  // }
+
+  //オーバーライド（上書き）
+  public function getProduct();
+}
+
+interface NewsInterface{
+  //変数　関数
+  // public function echoProduct(){
+  //   echo '親クラスです';
+  // }
+
+  public function getProduct();
+}
+
+
+//具象クラス・親クラス・基底クラス・スーパークラス
+class BaseProduct{
+  //変数　関数
+  public function echoProduct(){
+    echo '親クラスです';
+  }
+
+  //オーバーライド（上書き）
+  public function getProduct(){
+    echo '親の関数です';
+  }
+}
+
+//子クラス・派生クラス・サブクラス
+class Product implements ProductInterface,NewsInterface {
+
+  //アクセス修飾子, private(外からアクセスできない), protected(自分・継承したクラス), public(公開)
+
+  //変数
+  private $product = [];
+
+  //関数
+
+  //初回
+  function __construct($product = null){
+
+    $this->product = $product;
+  }
+
+  public function getProduct(){
+    echo $this->product;
+  }
+
+  public function addProduct($item){
+    $this->product .= $item;
+  }
+
+  public function getNews(){
+    echo 'ニュースです';
+  }
+
+  public static function getStaticProduct($str){
+    echo $str;
+  }
+
+}
+
+$instance = new Product('テスト');
+
+var_dump($instance);
+
+$instance->getProduct();
+echo '<br>';
+
+//親クラスのメソッド
+// $instance->echoProduct();
+// echo '<br>';
+
+$instance->addProduct('追加分');
+echo '<br>';
+
+$instance->getProduct();
+echo '<br>';
+
+//静的(static) クラス名::関数名
+Product::getStaticProduct('静的');
+echo '<br>';
